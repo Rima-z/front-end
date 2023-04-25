816991371e854966c6baf92f0165cfeb0de61513
Rima-z <rimazaghden057@gmail.com>
2023-04-25 11:43:30 +0100

    ajouter pop-up pour le profil

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
 
+import { Router } from '@angular/router';
+
+
 @Component({
   selector: 'app-test',
   templateUrl: './test.component.html',
   styleUrls: ['./test.component.css']
 })
 export class TestComponent {
+  constructor(private router: Router) { }
+  redirectToProfil() {
+    this.router.navigate(['/profil'], { replaceUrl: true });
+  }
+  redirectToDashboard() {
+    this.router.navigate(['/dashboard'], { replaceUrl: true });
+  }
+  redirectToTraitement() {
+    this.router.navigate(['/traitement'], { replaceUrl: true });
+  }
+  redirectToLogin() {
+    this.router.navigate(['/login'], { replaceUrl: true });
+  }
+  redirectToHistoriquec() {
+    this.router.navigate(['/historiquec'], { replaceUrl: true });
+  }
+  redirectToHistoriqued() {
+    this.router.navigate(['/historiqued'], { replaceUrl: true });
+  }
+  redirectToSugrec() {
+    this.router.navigate(['/sugrec'], { replaceUrl: true });
+  }
+  
 
 }
